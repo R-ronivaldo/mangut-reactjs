075c4ea9137a2a6a3db26b8e8028d143922653d1
junhfer <juniorferreira59@outlook.com>
2020-07-07 05:13:23 -0300

    adicionando localStorage e tela de loading

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -2,11 +2,17 @@ import React, { useContext } from 'react';
 import { UserContext } from '../../contexts/userContext';
 
 const User = () => {
-    const { user, token } = useContext(UserContext);
+    const { user, token, singOut } = useContext(UserContext);
+
+    function handleSignOut() {
+        singOut();
+    }
+
     return (
         <div>
             <div key={user.id}>Nome: {user.name} - Sobrenome: {user.secondName} - Email: {user.email} - Cpf: {user.cpf}</div>
             <div key={user.id}>Token: {token}</div>
+            <div><button onClick={handleSignOut}>Sing out</button></div>
         </div>
     );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,20 @@ import { UserContext } from '../contexts/userContext';
 import RoutesLogin from './RoutesLogin';
 import RoutesApp from './RoutesApp';
 
+import ReactLoading from 'react-loading';
+
 const Routes = () => {
-    const { user, signed, token } = useContext(UserContext);
+    const { user, signed, token, loading } = useContext(UserContext);
     console.log(signed);
     console.log(token);
     console.log(user);
+
+    if(loading) {
+        return (
+            <ReactLoading type={'spin'} color={'#333'} height={80} width={50} />
+        );
+    }
+
     return signed ? <RoutesApp /> : <RoutesLogin />
 };
 
